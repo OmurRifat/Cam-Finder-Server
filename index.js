8e20bf7eb1fcc8d7dec7f3e142b65eb305ec05bf
OmurRifat <jabedomur.ece.ku@gmail.com>
2022-11-24 22:17:12 +0600

    api for homepage product created

--- index.js
+++ index.js
@@ -6,9 +6,36 @@ const cors = require('cors');
 require('dotenv').config();
 const port = process.env.PORT || 5000;
 const app = express();
+const { MongoClient, ServerApiVersion } = require('mongodb');
+
+//user: camFinderManager
+//pass: zLTDhffu5CVYS4Sa
 
 app.use(cors());
 app.use(express.json());
+
+
+const uri = process.env.REACT_APP_url;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+// console.log(uri)
+const run = async () => {
+    try {
+        const cameraCollections = client.db("camFinder").collection("cameras");
+
+        app.get('/home-cameras', async (req, res) => {
+            const query = {};
+            const data = await (await cameraCollections.find(query).toArray()).splice(0, 3);
+            res.send(data)
+        })
+
+    }
+    finally {
+
+    }
+}
+run().catch(err => console.log(err.message));
+
+
 
 app.get('/', (req, res) => {
     res.send("Server is running");
